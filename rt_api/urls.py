4c0669d1ff7ccec595979d8ce886ceb737d38adb
Juan Ernesto Rosales Anzora <2504022019@mail.utec.edu.sv>
2025-08-30 15:36:57 -0600

    chore(api): remove unused include to satisfy ruff

--- rt_api/urls.py
+++ rt_api/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from apps.common import views as common_views
